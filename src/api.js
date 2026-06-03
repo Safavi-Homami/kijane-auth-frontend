5133241be4d5bfac55148628d27cc8094b7536f3
Safavi-Homami <safawiden@gmx.net>
2026-06-04 01:23:30 +0200

    Use Vite API base URL environment variable

--- src/api.js
+++ src/api.js
@@ -3,8 +3,12 @@ import axios from "axios";
 
 
 // ---- Axios-Instanz ---------------------------------------------------------
-const baseURL = "http://localhost:8080/api";
+//const baseURL = "http://localhost:8080/api";
 //const baseURL = "https://kijane-auth-backend-production.up.railway.app/api";
+
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+
 
 const api = axios.create({ baseURL });
 
